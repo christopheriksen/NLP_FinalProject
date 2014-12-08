30483786e0576cad6f3bdfd82acd76c0a5ef1d6c
TheFuzz1991 <jamesreinke91@gmail.com>
2014-12-08 14:14:43 -0800

    added more lucene functionality

--- src/Java/lucene/run.sh
+++ src/Java/lucene/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -vx
-javac ./src/*.java -d './class' -cp './jars/*'
+javac -cp ./jars/lucene_core.jar:./jars/lucene_analyzer.jar:./jars/lucene_query_parser.jar ./src/*.java -d './class'
 java -Xms4g -Xmx4g -cp './class:./jars/*' TextFileIndexer
--- src/Java/lucene/src/TextFileIndexer.java
+++ src/Java/lucene/src/TextFileIndexer.java
@@ -1,5 +1,3 @@
-package com.lucenetutorial.apps;
-
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
